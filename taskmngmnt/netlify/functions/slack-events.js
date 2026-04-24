58778d1428590d08b8062f88aa260b269e85cd24
DanielRiddle-goodfolk <daniel@goodfolk.com>
2026-04-24 12:27:07 -0400

    update slack

--- taskmngmnt/netlify/functions/slack-events.js
+++ taskmngmnt/netlify/functions/slack-events.js
@@ -1,5 +1,14 @@
 exports.handler = async (event) => {
-  const body = JSON.parse(event.body);
+  if (!event.body) {
+    return { statusCode: 200, body: 'OK' };
+  }
+
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch(e) {
+    return { statusCode: 200, body: 'OK' };
+  }
 
   // Handle Slack's challenge verification
   if (body.type === 'url_verification') {
@@ -11,7 +20,7 @@ exports.handler = async (event) => {
 
   // Forward app_mention events to Zapier
   if (body.event && body.event.type === 'app_mention') {
-    await fetch('https://hooks.zapier.com/hooks/catch/3282298/uvyxc1x/', {
+    await fetch('YOUR_ZAPIER_CATCH_HOOK_URL', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body.event)
